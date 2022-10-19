61d912134ee94589ede055fc86d9d525e1e49070
nameing <>
2022-10-19 11:00:46 +0900

    Stack
    Stack

---
+++ JavaExe/src/D_1019/Stack01.java
@@ -0,0 +1,36 @@
+package D_1019;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stack01 {
+
+    private int[] arr = new int[10000];
+    private List<Integer> arl = new ArrayList<>();
+
+    private int pointer = 0;
+
+    public Stack01(){
+    }
+
+    public Stack01(int size){
+        this.arr = new int[size];
+    }
+
+    public void push(int value){
+        this.arr[this.pointer] = value;
+        this.pointer++;
+    }
+
+    public int pop(){
+        int value = this.arr[this.pointer-1];
+        this.pointer--;
+        return value;
+    }
+
+    public int[] getArr(){
+        return arr;
+    }
+
+
+}
